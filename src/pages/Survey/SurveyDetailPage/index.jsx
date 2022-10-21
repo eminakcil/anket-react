ac4fd7ac6d9a0a1d28865c13b97dbceb0690e5ed
eminakcil <eminakcil@outlook.com>
2022-10-21 17:19:23 +0300

    survey detail page stats link

--- src/pages/Survey/SurveyDetailPage/index.jsx
+++ src/pages/Survey/SurveyDetailPage/index.jsx
@@ -1,10 +1,13 @@
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { SurveyService } from '../../../services'
 import Loading from '../../../components/Loading'
 import TextQuestion from './components/TextQuestion.jsx'
 import SelectQuestion from './components/SelectQuestion.jsx'
 import RateQuestion from './components/RateQuestion.jsx'
+import { chartIcon } from '../../../icons'
+import Button from '../../../components/Button'
+import { getPath } from '../../../utils'
 
 const SurveyDetailPage = () => {
   const { id } = useParams()
@@ -30,8 +33,22 @@ const SurveyDetailPage = () => {
 
   return (
     <>
-      {/* <pre>{JSON.stringify(survey)}</pre> */}
       <div className="flex flex-col gap-3">
+        <div className="flex">
+          <Button
+            as={Link}
+            to={getPath('surveys.statistics')}
+            className="ml-auto"
+            variant="green"
+          >
+            {chartIcon}
+          </Button>
+        </div>
+        <img
+          className="justify-self-center w-full max-w-xl self-center"
+          src={'http://localhost:3000'.concat(survey.logo.path)}
+          crossOrigin="anonymous"
+        />
         <span className="text-2xl font-medium self-center">{survey.title}</span>
         <div className="flex flex-col py-3 px-2 border border-solid border-gray-200 rounded-xl">
           <span className="font-medium">Selamlama / Giriş / Açıklama</span>
